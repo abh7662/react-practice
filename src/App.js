987dd3a11aedc9faea25f6d39ac9cfb66e4c178a
ak7662 <ak7662@gmail.com>
2021-05-11 22:35:58 +0530

    todos app started

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from "react"
 import "./App.css"
 
-import Navbar from "./Navbar"
-import MainContent from "./MainContent"
-import Footer from "./Footer"
 
-// Set up the React app from scratch
-// Render an App component (defined in a separate file)
-// Inside App, render:
-  // 1. A Navbar component
-  // 2. A MainContent component
-  // 3. A Footer component
+// From scratch, initialize the React app
+// Render an <App /> component
+// Create the <App /> component from scratch
+// Have the <App /> component render 3 or 4 checkboxes with paragraphs or spans next to it
+// like you're making a todo list with some hard-coded items on it
 const App = () => (
 <div>
-    <Navbar />
-    <MainContent />
-    <Footer />
+    <input type="checkbox" />
+    <label>Mango</label><br />
+
+    <input type="checkbox" />
+    <label>Mango</label><br />
+
+    <input type="checkbox" />
+    <label>Mango</label><br />
+
+    <input type="checkbox" />
+    <label>Mango</label><br />
+    
 </div>
 )  
 export default App
