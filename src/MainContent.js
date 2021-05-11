a54cc4a1d45d26468e525b005e4e2c360b464f0a
ak7662 <ak7662@gmail.com>
2021-05-11 20:50:42 +0530

    src----setting_header_main_footer

---
+++ src/MainContent.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const MainContent = () => (
+    <main>
+        <h1>Main</h1>
+        <h1>Main</h1>
+        <h1>Main</h1>
+        <h1>Main</h1>
+        <h1>Main</h1>
+        <h1>Main</h1>
+        <h1>Main</h1>
+        <h1>Main</h1>
+        <h1>Main</h1>
+        <h1>Main</h1>
+        </main>
+)
+
+export default MainContent
