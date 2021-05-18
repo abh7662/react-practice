e60e562a73247bb2b23341673111eb1dc1fe6c23
ak7662 <ak7662@gmail.com>
2021-05-18 22:09:20 +0530

    added event hangelling in todos app

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,9 +1,13 @@
 import React from "react"
 
+
 function TodoItem(props) {
     return (
         <div className="todo-item">
-            <input type="checkbox" checked = {props.completed} />
+            <input type="checkbox"
+                checked = {props.completed} 
+                onChange={() => console.log("clicked")}
+            />
             <p>{props.text}</p>
         </div>
     )
