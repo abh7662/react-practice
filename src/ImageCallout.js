7fb7665555197582c3b64249b38c467e86426149
ak7662 <ak7662@gmail.com>
2021-05-22 20:15:34 +0530

    React Children

---
+++ src/ImageCallout.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function ImageCallout(props) {
+    return (
+        <div className="callout">
+            {props.children}
+        </div>
+    )
+}
+
+export default ImageCallout
