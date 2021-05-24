eca33fa16d65fe125d97163cbbb9c1754189aef8
ak7662 <ak7662@gmail.com>
2021-05-24 18:47:37 +0530

    togglerUsingCustomHook

---
+++ src----togglerUsingCustomHook/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Menu from "./Menu"
+import Favourite from "./Favourite"
+
+function App() {
+    return (
+        <div>
+            <Menu />
+            <hr />
+            <Favourite />
+        </div>
+    )
+}
+
+export default App
