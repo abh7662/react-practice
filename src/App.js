9e3e857feccdb75261c922d3bafd1e4ebb022364
ak7662 <ak7662@gmail.com>
2021-06-12 20:31:59 +0530

    basic redux example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React, { useContext } from 'react';
-import Redux from "redux"
+import { createStore } from 'redux'
+
+function counterReducer(state = { value: 0 }, action) {
+  switch (action.type) {
+    case 'counter/incremented':
+      return { value: state.value + 1 }
+    case 'counter/decremented':
+      return { value: state.value - 1 }
+    default:
+      return state
+  }
+}
+let store = createStore(counterReducer)
+store.subscribe(() => console.log(store.getState()))
+store.dispatch({ type: 'counter/incremented' })
+store.dispatch({ type: 'counter/incremented' })
+store.dispatch({ type: 'counter/decremented' })
 function App(){
-  const increment = () =>{
-    return{
-      type: 'INCREMENT'
-    }
-  }
-  const reducer = (state = {count:0},action) =>{
-    switch(action.type){
-      case 'INCREMENT':
-        return {
-          count: state.count + 1
-        }
-      default: 
-        return state
-    }
-  }
-  const store = Redux.createStore(reducer)
-  store.dispatch(increment())
-  console.log(count)
+  // const increment = () =>{
+  //   return{
+  //     type: 'INCREMENT'
+  //   }
+  // }
+  // const reducer = (state = {count:0},action) =>{
+  //   switch(action.type){
+  //     case 'INCREMENT':
+  //       return {
+  //         count: state.count + 1
+  //       }
+  //     default: 
+  //       return state
+  //   }
+  // }
+  // const store = Redux.createStore(reducer)
+  // store.dispatch(increment())
+  // console.log(count)
   return (
   <>
   </>    
@@ -26,6 +42,8 @@ function App(){
 }
 
 export default App
+
+
 
 
 
