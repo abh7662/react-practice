8ce891d136c9d8b8ff54d8afbbd6d96a663b7b51
ak7662 <ak7662@gmail.com>
2021-07-14 22:48:22 +0530

    js challenges

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ import Favorite from "./Favorite"
 import Toggler from "./Toggler"
 
 function App() {
+    var removeElement = function (nums, val) {
+
+        let k = 0
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] === val) {
+                nums[i] = nums[i + 1]
+                k++
+                if (nums[i + 1] === val) {
+                    i--
+                }
+            }
+        }
+        console.log(nums)
+        console.log(k)
+        return k
+
+    };
+
+    removeElement([1, 2, 2, 2, 3], 2)
     return (
         <div>
             <Menu />
