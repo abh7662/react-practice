1057d969dacbed32661151150002f53bfae2f213
ak7662 <ak7662@gmail.com>
2021-07-15 23:04:51 +0530

    practice on context api

--- src/App.js
+++ src/App.js
@@ -1,50 +1,9 @@
 import React from "react"
-import Menu from "./Menu"
-import Favorite from "./Favorite"
-import Toggler from "./Toggler"
 
 function App() {
-    var removeElement = function (nums, val) {
-
-        let k = 0
-        for (let i = 0; i < nums.length; i++) {
-            if (nums[i] === val) {
-                nums[i] = nums[i + 1]
-                k++
-                if (nums[i + 1] === val) {
-                    i--
-                }
-            }
-        }
-        console.log(nums)
-        console.log(k)
-        return k
-
-    };
-
-    removeElement([1, 2, 2, 2, 3], 2)
     return (
         <div>
-            <Menu />
-            <hr />
-            <Favorite />
-            {/* <Toggler defaultOnValue={true} render={
-                (on, toggle) => {
-                    return (
-                        <Menu on={on} toggle={toggle} />
-                    )
-                }
-            } />
-
-            <hr />
-            <Toggler defaultOnValue={false} render={
-                (on, toggle) => {
-                    return (
-                        <Favorite on={on} toggle={toggle} />
-                    )
-                }
-            } /> */}
-
+            <h1>hello</h1>
         </div>
     )
 }
