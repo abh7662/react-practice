9b91533343add02e46c9803228cc453bb27b2d06
ak7662 <ak7662@gmail.com>
2021-07-20 12:30:45 +0530

    redux practice

--- src/App.js
+++ src/App.js
@@ -1,40 +1,89 @@
+// import React from "react"
+// import { createStore } from "redux"
+// function App() {
+//     const initialState = {
+//         count: 0
+//     }
+//     const reducer = (state = initialState, action) => {
+//         switch (action.type) {
+//             case "INCREMENT":
+//                 return {
+//                     count: state.count + 1
+//                 }
+//             case "DECREMENT":
+//                 return {
+//                     count: state.count - 1
+//                 }
+//             case "DOUBLE":
+//                 return {
+//                     count: state.count * 2
+//                 }
+//         }
+//     }
+
+//     const store = createStore(reducer)
+
+//     store.subscribe(() => {
+//         console.log(store.getState())
+//     })
+
+//     store.dispatch({ type: "INCREMENT" })
+//     store.dispatch({ type: "INCREMENT" })
+//     store.dispatch({ type: "DOUBLE" })
+//     store.dispatch({ type: "DECREMENT" })
+//     return (
+//         <div>
+//             hello
+//         </div>
+//     )
+// }
+
+
+
+// export default App
+
+
 import React from "react"
-import { Redux } from "redux"
-function App() {
+import { createStore } from "redux"
+
+export default function App() {
     const initialState = {
         count: 0
     }
+
     const reducer = (state = initialState, action) => {
         switch (action.type) {
-            case "INCREMENT":
+            case "INC":
                 return {
                     count: state.count + 1
                 }
-			case "DECREMENT":
+            case "DEC":
                 return {
                     count: state.count - 1
                 }
-			case "DOUBLE":
+            case "DOU":
                 return {
-                    count: state.count - 1
+                    count: state.count * 2
                 }
+            default: {
+                return state
+            }
         }
     }
-
-    const store = Redux.createStore(reducer)
+    const store = createStore(reducer)
 
     store.subscribe(() => {
         console.log(store.getState())
     })
 
-    store.dispatch({ type: "INCREMENT" })
+    store.dispatch({ type: "INC" })
+    store.dispatch({ type: "DEC" })
+    store.dispatch({ type: "INC" })
+    store.dispatch({ type: "DOU" })
+
     return (
         <div>
-            hello
+
         </div>
     )
 }
-
-
-
-export default App
