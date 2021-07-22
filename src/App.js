fcee3e8b8914c4abbadb1be9437e924b4db16845
ak7662 <ak7662@gmail.com>
2021-07-22 14:28:44 +0530

    checking a panagram

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import React from "react"
 
 
 export default function App() {
-    console.log(Math.round(.1 + .2))
+    function isPanagram(str) {
+        const allAlphabets = 'abcdefghijklmnopqrstuvwxyz'.split('')
+        let count = 0
+        for (let i = 0; i < 26; i++) {
+            if (str.toLowerCase().includes(allAlphabets[i])) {
+                count++
+            }
+        }
+        if (count === 26) {
+            return true
+        }
+        return false
+    }
+
+    console.log(isPanagram("The quick Brown fox jumps Over the lazy dog"))
     return (
         <div>
             hello
