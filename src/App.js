437e2c6bb9d06723a94b694647479c2cc115a94b
ak7662 <ak7662@gmail.com>
2021-07-22 21:00:08 +0530

    removing duplicates from array using reduce

--- src/App.js
+++ src/App.js
@@ -3,45 +3,19 @@ import React from "react"
 
 
 export default function App() {
-    // const getCount = (value, arr) => {
-    //     const newArr = arr.filter(val => val === value)
-    //     return newArr.length
-    // }
-    // const encode = (string) => {
-    //     const StrArr = string.split('')
-    //     const uniqueStrArr = [... new Set(StrArr)]
-    //     let result = []
-    //     for (let x of uniqueStrArr) {
-    //         result.push(getCount(x, StrArr))
-    //         result.push(x)
-    //     }
-    //     return result.join('')
-    // }
-
-    // const decode = (string) => {
-    //     const strArr = string.split('')
-    //     let result = ''
-    //     for (let i = 0; i < strArr.length; i + 2) {
-    //         // console.log(strArr[i])
-    //         // console.log(strArr[i + 1])
-    //     }
-    // }
-
-
-    // console.log(encode('wwwiiuuuu')) // 3w2i4u
-    // console.log(decode('2u3a4o')) // uuaaaoooo 
-
-    const encode = (string) => {
-        return string.replace(/(\w)\1+/g, (m, v) => `${m.length}${v}`)
+    const removeDuplicateValues = (array) => {
+        return array.reduce((accumulator, value) => {
+            if (!accumulator.includes(value)) {
+                accumulator.push(value)
+            }
+            return accumulator
+        }, [])
     }
+    console.log(
+        removeDuplicateValues(["one", "two", "one", "three", "three", "two"])
+    ); // ['one','two','three']
 
-    const decode = (string) => {
-        return string.replace(/(\d+)(\w)/g, (x, y, z) => z.repeat(y))
-    }
 
-
-    console.log(encode('wwwwwwwwiiuuuu')) // 3w2i4u
-    console.log(decode('2u3a4o')) // uuaaaoooo 
     return (
         <div>
             hello
