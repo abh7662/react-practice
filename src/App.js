00b1286e71dc58f14c80070184eeebdd8f875fce
ak7662 <ak7662@gmail.com>
2021-07-22 23:18:30 +0530

    transpose of a matrix

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import React from "react"
 
 
 export default function App() {
-    const makeFlat = (array) => {
-        return array.reduce((acc, curr) => {
-            return Array.isArray(curr) ? [...acc, ...makeFlat(curr)] : [...acc, curr]
-        }, [])
+    const transposeMatrix = (array) => {
+        return array[0].map((_, i) => array.map(v => v[i]))
     }
 
 
-    console.log(makeFlat(["one", ["two", "three"], ["four", ["five"]]])); // ['one','two','three','four','five']
+    console.log(transposeMatrix([
+        [1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3],
+    ]))
+    // [[1,1,1],[2,2,2],[3,3,3]]
     return (
         <div>
             hello
