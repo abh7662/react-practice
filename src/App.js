5e92c3d07eaa052904d4d4050cdc3dd2fbdd0825
ak7662 <ak7662@gmail.com>
2021-07-27 22:39:54 +0530

    count function

--- src/App.js
+++ src/App.js
@@ -2,21 +2,10 @@
 import React from "react"
 
 export default function App() {
-    function count1() {
-        let i = 0
-        i = i + 1
-        return function () {
-            return i
-        }
-    }
-    const count = count1()
-    count()
-    console.log(count());
-    console.log(count());
-    console.log(count());
+
     return (
         <div>
-            hello
+            <h1>hello</h1>
         </div>
     )
 }
