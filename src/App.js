ff2b524ec78e84feef64688987da86cb7cc27e1c
ak7662 <ak7662@gmail.com>
2021-08-02 19:12:18 +0530

    rakuten

--- src/App.js
+++ src/App.js
@@ -1,17 +1,129 @@
 
-import React from "react"
-import RenderProps from "./renderProps"
-function App() {
+import React, { useEffect, useState } from "react"
 
+class App1 extends React.Component {
+    constructor() {
+        super()
+        console.log("hello")
+    }
+    componentDidMount() {
+        console.log("mount")
+    }
+    render() {
+        console.log("render")
+        return (
+            <div>
+
+            </div>
+        )
+
+    }
+}
+
+export default function App() {
+
+
+
+    // 'use-strict';
+    // console.log(a);
+    // var a = 10;
+    // console.log(a);
+    'use-strict';
+    console.log(a);
+    a = 10;
+    console.log(a);
+
+    const a = [1, 2, 3, {
+        c: {
+            d: 4
+        }
+    }];
+    let b = […a];
+    a[3].c.d = 6;
+
+    console.log(a);
+    console.log(b);
+
+
+
+    const [value, setValue] = useState(0);
+    useEffect(() => {
+        // setValue(1);
+        // setValue(2);
+        // setValue(3);
+        // setValue(4);
+        // let printValue = ""
+
+
+        // setInterval(() => {
+        //     setValue(prevValue => {
+        //         return prevValue + 1
+        //     })
+
+        //     console.log(value)
+        // }, 1000)
+        // setValue(printValue)
+        console.log('I am child');
+    }, []);
+    return (<div>
+        {/* {value} */}
+    </div>)
+}
+
+
+
+// return (
+//     <div>
+//         {/* <ParentComponent></ParentComponent> */}
+//     </div>
+// )
+// }
+
+
+function ParentComponent() {
+
+    const [counter, setCounter] = useState(0)
+    useEffect(() => {
+        console.log('I am parent');
+    }, []);
+    const handleChange = () => {
+        setCounter(counter + 1)
+    }
     return (
-        <div>
-            <RenderProps render={(val) =>
-                <>
-                    {`value is ${val}`}
-                </>
-            } />
-        </div>
+        <>
+
+            <button onClick={handleChange}>increment</button>
+            {counter % 2 === 0 ? <ChildComponent counter={counter}></ChildComponent> : ""}
+        </>
+
     )
 }
 
-export default App
+// function ChildComponent(props) {
+//     useEffect(() => {
+//         console.log('I am child');
+//     }, []);
+//     return (<div>{props.counter}</div>)
+// }
+
+class ChildComponent extends React.Component {
+    constructor() {
+        super()
+
+    }
+    shouldComponentUpdate(state, props) {
+        console.log(state)
+        console.log(props)
+    }
+    render() {
+        return (
+            <div>
+                {this.props.counter}
+            </div>
+        )
+    }
+}
+
+// mount --  render()  
+
+// update -- componentdidupdate unmonunr
--- src/index.js
+++ src/index.js
@@ -5,4 +5,5 @@ import "./styles.css"
 
 ReactDOM.render(
     <App />
+
     , document.getElementById("root"))
