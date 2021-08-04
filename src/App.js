28f0ff227e441e5c53736169fa3e5c05d004a98c
ak7662 <ak7662@gmail.com>
2021-08-04 10:32:35 +0530

    use of progress bar and datalist

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 
 export default function App() {
-    function sayHello(a, b) {
-        return `hello ${a} ${b} ${this.name}`
-    }
+    const [val, setVal] = useState(0.2)
+    useEffect(() => {
+        setInterval(() => {
+            setVal(prevVal => prevVal + .1)
+        }, 10)
 
-    const myObj = { name: "monu" }
-    const myObj1 = { name: "minu" }
+    }, [])
 
-    console.log(sayHello.call(myObj, "shfbshdfbsdj", "abc"))
-    console.log(sayHello.apply(myObj1, ["a", "b"]))
-
-    const myFun = sayHello.bind(myObj, "ram", "shyam")
-    console.log(myFun())
     return (
         <div>
+            <p>Disk Usage: <meter value={val} min="0" max="100">20%</meter></p>
+            <p>Total Score: <meter value="6" min="0" max="10">6 out of 10</meter></p>
+            <p>Pollution Level: <meter low="60" high="80" max="100" value="85">Very High</meter></p>
+
+            <p>Enter your favorite browser name:</p>
+            <input type="text" list="browsers" name="favorite_browser" />
+            <datalist id="browsers">
+                <option value="Firefox" />
+                <option value="Chrome" />
+                <option value="Internet Explorer" />
+                <option value="Opera" />
+                <option value="Safari" />
+            </datalist>
         </div>
     )
 }
+
